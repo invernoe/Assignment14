12b4f5a736828f73ee78336273102429dadd33c9
invernoe <mahmoudk221999@gmail.com>
2024-08-05 21:39:09 +0300

    Add project files.

---
+++ Assignment14/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var oddNum = list.Where(n => n % 2 == 0);
+            foreach (var n in oddNum)
+            {
+                Console.WriteLine(n);
+            }
+            list.AddRange([11, 12, 13, 14, 15]);
+            Console.WriteLine("========");
+            foreach (var n in oddNum)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
